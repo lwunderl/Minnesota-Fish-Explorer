63210a5ffb4aa611c07aabb0ab0783dc2f21cc4e
lwunderl <luke.wunderlin@gmail.com>
2023-03-29 18:11:46 -0500

    working on consolidating header names

--- Fish_Survey/fish_finder_db_schema.sql
+++ Fish_Survey/fish_finder_db_schema.sql
@@ -1,33 +1,94 @@
 --Drop Tables
-DROP TABLE if EXISTS cpue, lengths, fish, lakes, water_access_sites;
+DROP TABLE if EXISTS water_access_sites, median_cpue, fish_catch, fish_length, lake_id, fish_id;
 
---Create CPUE table
-CREATE TABLE cpue(
-
+--Create Fish ID table
+CREATE TABLE fish_id (
+    fish_description VARCHAR(30),
+    fish_id PRIMARY KEY
 );
 
---Create Length table
-CREATE TABLE lengths (
-
+--Create Lake ID table
+CREATE TABLE lake_id (
+    lake_id VARCHAR(10) PRIMARY KEY,
+    lake_name VARCHAR(30),
+    county VARCHAR(25),
+    nearest_town VARCHAR(30),
+    lake_area DECIMAL,
+    littoral_area DECIMAL,
+    lake_depth INTEGER,
+    mean_depth DECIMAL,
+    water_access_sites VARCHAR(7),
+    longitude DECIMAL,
+    latitude DECIMAL,
+    FOREIGN KEY (water_access_sites) REFERENCES water_access_sites(site_id)
 );
 
---Create Fish table
-CREATE TABLE fish (
-
+--Create Fish Length table
+CREATE TABLE fish_length (
+    lake_id VARCHAR(10),
+    species VARCHAR(3),
+    fish_count VARCHAR(250),
+    max_length INTEGER,
+    min_length INTEGER,
+    survey_id VARCHAR(30) PRIMARY KEY,
+    survey_date DATE,
+    average_length DECIMAL,
+    FOREIGN KEY (lake_id) REFERENCES lake_id(lake_id),
+    FOREIGN KEY (species) REFERENCES fish_id(fish_id)
 );
 
---Create Lakes table
-CREATE TABLE lakes (
+--Create Fish Catch table
+CREATE TABLE fish_catch (
+    lake_id VARCHAR(10),
+    survey_cpue DECIMAL,
+    average_weight DECIMAL,
+    gear VARCHAR(30),
+    gear_count INTEGER,
+    species VARCHAR(3),
+    survey_id VARCHAR(30) PRIMARY KEY,
+    survey_date DATE,
+    total_catch INTEGER,
+    total_weight DECIMAL,
+    lower_quartile_count DECIMAL,
+    upper_quartile_count DECIMAL,
+    lower_quartile_weight DECIMAL,
+    upper_quartile_weight DECIMAL,
+    FOREIGN KEY (lake_id) REFERENCES lake_id(lake_id),
+    FOREIGN KEY (species) REFERENCES fish_id(fish_id)
+);
 
+--Create Median CPUE table
+CREATE TABLE median_cpue(
+    lake_id VARCHAR(10),
+    species VARCHAR(3),
+    lake_cpue DECIMAL,
+    PRIMARY KEY (lake_id, species)
+    FOREIGN KEY (lake_id) REFERENCES lake_id(lake_id),
+    FOREIGN KEY (species) REFERENCES fish_id(fish_id)
 );
 
 --Create WAS table
 CREATE TABLE water_access_sites (
-
+    site_id VARCHAR(7) PRIMARY KEY,
+    site_name VARCHAR(30),
+    directions_to_site VARCHAR(250),
+    administrator_of_site VARCHAR(100),
+    number_of_docks INTEGER,
+    number_of_restrooms INTEGER,
+    number_of_parking_lots INTEGER,
+    number_of_accessible_spaces INTEGER,
+    number_of_trailer_spaces INTEGER,
+    number_of_vehicle_spaces INTEGER,
+    parking_lot_surface VARCHAR(12),
+    number_of_ramps INTEGER,
+    ramp_surface VARCHAR(12),
+    longitude DECIMAL,
+    latitude DECIMAL
 );
 
-SELECT * FROM cpue;
-SELECT * FROM lengths;
-SELECT * FROM fish;
-SELECT * FROM lakes;
+SELECT * FROM fish_id;
+SELECT * FROM lake_id;
+SELECT * FROM fish_length;
+SELECT * FROM fish_catch;
+SELECT * FROM median_cpue;
 SELECT * FROM water_access_sites;
